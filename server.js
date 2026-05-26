437734bdc6eb4a3c2218485440e29610fb6708ee
KoushikPanda1729 <programmer747767@gmail.com>
2026-05-26 09:43:07 +0530

    fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { exec } = require("child_process");
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
